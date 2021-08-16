7a504c46a7e1353fb6cb409435d13b7a71555c49
hieunv-tsdv <hieu2.nguyenvan@toshiba.co.jp>
2021-08-16 11:47:17 +0700

    Update mkfs.fat.c
    mkfs.fat: don't need to check volume_id when SOURCE_DATE_EPOCH was set

--- src/mkfs.fat.c
+++ src/mkfs.fat.c
@@ -1077,7 +1077,7 @@ static void setup_tables(void)
         }
 
         /* If is not available then generate random 32 bit disk signature */
-        if (invariant || (getenv("SOURCE_DATE_EPOCH") && volume_id))
+        if (invariant || getenv("SOURCE_DATE_EPOCH"))
             disk_sig = volume_id;
         else if (!disk_sig)
             disk_sig = generate_volume_id();
